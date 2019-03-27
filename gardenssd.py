d2f5dd84e215c91db30bfb84f5363c2ec0fef878
Timothy Spann <tspann@hortonworks.com>
2019-03-27 16:10:07 -0400

    Create gardenssd.py

---
+++ gardenssd.py
@@ -0,0 +1,36 @@
+from __future__ import print_function
+import pandas as pd
+import sys, re
+from operator import add
+from pyspark.sql import SparkSession
+import matplotlib.pyplot as plt
+
+plt.close('all')
+  
+pd.options.display.html.table_schema = True
+  
+spark = SparkSession\
+  .builder\
+  .appName("Visualize garden data")\
+  .getOrCreate()
+
+# Access the parquet  
+# garden = spark.read.parquet("/tmp/garden/*.parquet")
+# gdata = garden.toPandas()
+# df = pd.DataFrame(gdata)
+
+# df.plot()
+
+# df.plot.bar()
+
+# Access the parquet  
+gtf = spark.read.parquet("/tmp/gardentf/*.parquet")
+gtfdata = gtf.toPandas()
+pd.DataFrame(gtfdata)
+
+# Access the parquet  
+gssd = spark.read.parquet("/tmp/gardenssd/*.parquet")
+gssddata = gssd.toPandas()
+pd.DataFrame(gssddata)
+
+spark.stop()
